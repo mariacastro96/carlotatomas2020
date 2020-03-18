713c5826b8c6e44d1244e7577cd47c1f0946ff40
Maria Castro <mariacantocastro@gmail.com>
2020-03-18 23:17:37 +0000

    festa + missa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def countdown
   end
+  
+  def homepage
+  end
+
+  def missa
+  end
+
+  def festa
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # get 'pages/countdown', to: 'pages#countdown'
+  get 'pages/homepage', to: 'pages#homepage'
+  get 'pages/missa', to: 'pages#missa'
+  get 'pages/festa', to: 'pages#festa'
   root to: 'pages#countdown'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
